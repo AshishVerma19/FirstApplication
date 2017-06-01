6ad0267b69f8853cb991a53b89396dbbbfb0e820
Ashish <Ashish@ASHISHPC>
2017-06-01 05:47:21 +0530

    List of names Added.

--- FirstApplication/Scripts/AngularApp/mainModule.js
+++ FirstApplication/Scripts/AngularApp/mainModule.js
@@ -4,5 +4,5 @@ var app = angular.module("app", []);
 app.controller("IndexController", ["$scope", function ($scope) {
 
     $scope.name = "Ashish verma";
-
+    $scope.names = ['Amit', 'Ashish', 'Rohit', 'Aslam', 'Saurabh'];
 }]);
